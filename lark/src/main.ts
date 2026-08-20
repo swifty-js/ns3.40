5f37fdfd8d1620778f2a6abee84b0a95a591b452
hangtiancheng.htc <hangtiancheng.htc@taobao.com>
2026-08-20 20:35:24 +0800

    feat: Update frontend

--- lark/src/main.ts
+++ lark/src/main.ts
@@ -1,4 +1,46 @@
 import "./styles/global.css";
-import boot from "./boot";
 
-boot();
+import { Framework } from "@lark.js/mvc";
+import { enablePlugin, initLarkSentry } from "@lark.js/sentry";
+import {
+  ScreenRecordPlugin,
+  PerformancePlugin,
+  ExposurePlugin,
+} from "@lark.js/sentry/plugins";
+
+Framework.boot({
+  rootId: "app",
+  routeMode: "hash",
+  defaultPath: "/dashboard",
+  defaultView: "dashboard",
+  routes: {
+    "/dashboard": "dashboard",
+    "/scenario": "scenario",
+  },
+  unmatchedView: "dashboard",
+  require: async (names: string[]) =>
+    Promise.all(
+      names.map((n) => import(`./views/${n}.ts`).then((m) => m.default)),
+    ),
+});
+
+if (Framework.isBooted()) {
+  initLarkSentry({
+    dsn: "/ns3.40",
+    debug: true,
+    projectId: "ns3.40",
+    beforePushEventList(eventList) {
+      if (!import.meta.env.DEV) {
+        console.log("@swifty.js/sentry App:", eventList);
+        return false;
+      }
+      return eventList;
+    },
+  });
+
+  enablePlugin(
+    new ScreenRecordPlugin(),
+    new PerformancePlugin(),
+    new ExposurePlugin(),
+  );
+}
